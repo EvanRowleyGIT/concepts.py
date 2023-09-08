26050fe28694296e96044b901f5905eb5e3b6e44
EvanRowleyGIT <143126749+EvanRowleyGIT@users.noreply.github.com>
2023-09-08 11:22:25 -0600

    Add files via upload

--- consepts.py/Untitled-1.py
+++ consepts.py/Untitled-1.py
@@ -54,4 +54,25 @@ print(message)
 print(message2)
 print(not x)
 
-#PEMMDAs
+z = 7 // 2
+t = 7 / 2
+print(z)
+
+def my_function():
+    print("I am so great!")
+
+my_function()
+
+def add(num1, num2):
+    print(num1 + num2)
+
+my_name = "hi,my,name,is,bob"
+words = my_name.split(",")
+print(words)
+
+def subtract(num1, num2):
+    return num1 - num2
+
+difference = subtract(4,2)
+
+print (difference)
